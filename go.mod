2ebb7922acede81f30601bdbbfdee1bed179eccd
James Woolfenden <james.woolfenden@gmail.com>
2022-10-03 14:13:16 +0100

    repo secret example

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-github/v47 v47.1.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/jamesruan/sodium v1.0.14 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,60 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt" //nolint:goimports
 	"github.com/google/go-github/v47/github"
+	"golang.org/x/crypto/nacl/box"
 	"golang.org/x/oauth2"
 	"log"
 	"os"
 )
 
 func main() {
+	owner := "jameswoolfenden"
+	repository := "terraform-aws-activemq"
+	keyName := "HELLO"
+	keyText := "hello world"
+	response, err := SetRepoSecret(owner, repository, keyText, keyName)
+	if err != nil {
+		log.Print(response)
+		log.Print(err)
+	}
+}
+
+func SetRepoSecret(owner string, repository string, keyText string, keyName string) (*github.Response, error) {
+	keyID, publicKey, err := getPublicKeyDetails(owner, repository)
+
+	if err != nil {
+		return nil, err
+	}
+
+	encryptedBytes, err := encryptPlaintext(keyText, publicKey)
+
+	if err != nil {
+		return nil, err
+	}
+
+	encryptedValue := base64.StdEncoding.EncodeToString(encryptedBytes)
+
+	// Create an EncryptedSecret and encrypt the plaintext value into it
+	eSecret := &github.EncryptedSecret{
+		Name:           keyName,
+		KeyID:          keyID,
+		EncryptedValue: encryptedValue,
+	}
+
+	ctx, client := getGithubClient()
+
+	response, err := client.Actions.CreateOrUpdateRepoSecret(ctx, owner, repository, eSecret)
+
+	if err != nil {
+		return response, err
+	}
+	return response, nil
+}
+
+func getGithubClient() (context.Context, *github.Client) {
 	token := os.Getenv("GITHUB_TOKEN")
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
@@ -17,26 +64,39 @@ func main() {
 	tc := oauth2.NewClient(ctx, ts)
 
 	client := github.NewClient(tc)
+	return ctx, client
+}
 
-	james := github.RepositoryListOptions{"Public", "owner", "", "", "", github.ListOptions{Page: 4, PerPage: 50}}
-	// list all repositories for the authenticated user
-	repos, _, err := client.Repositories.List(ctx, "", &james)
+func getPublicKeyDetails(owner, repository string) (keyID, pkValue string, err error) {
+	ctx, client := getGithubClient()
+
+	publicKey, _, err := client.Actions.GetRepoPublicKey(ctx, owner, repository)
 	if err != nil {
-		log.Fatal(err)
+		return keyID, pkValue, err
 	}
 
-	for _, repo := range repos {
-		test := *repo
-		log.Printf(test.String())
+	return publicKey.GetKeyID(), publicKey.GetKey(), err
+}
+
+func encryptPlaintext(plaintext, publicKeyB64 string) ([]byte, error) {
+	publicKeyBytes, err := base64.StdEncoding.DecodeString(publicKeyB64)
+	if err != nil {
+		return nil, err
 	}
 
-	//temp := github.ActionsService{client}
-	temp2, _, _ := client.Actions.ListRepoSecrets(ctx, "jameswoolfenden", "terraform-aws-activemq", nil)
-	secrets := temp2.Secrets
-	for _, secret := range secrets {
-		temp3 := *secret
-		log.Printf(temp3.Name)
+	var publicKeyBytes32 [32]byte
+	copiedLen := copy(publicKeyBytes32[:], publicKeyBytes)
+	if copiedLen == 0 {
+		return nil, fmt.Errorf("could not convert publicKey to bytes")
 	}
 
-	client.Actions.CreateOrUpdateRepoSecret(ctx, "jameswoolfenden", "terraform-aws-activemq", "")
+	plaintextBytes := []byte(plaintext)
+	var encryptedBytes []byte
+
+	cipherText, err := box.SealAnonymous(encryptedBytes, plaintextBytes, &publicKeyBytes32, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return cipherText, nil
 }
